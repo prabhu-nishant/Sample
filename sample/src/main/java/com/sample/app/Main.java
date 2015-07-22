2abca77a6df9d8bcc6c786857754271f97159878
Nishant <prabhu.nishant@gmail.com>
2015-07-22 23:27:15 +0530

    Adding log4j

--- sample/src/main/java/com/sample/app/Main.java
+++ sample/src/main/java/com/sample/app/Main.java
@@ -1,11 +1,15 @@
 package com.sample.app;
 
+import org.apache.log4j.Logger;
+
 import com.sample.exception.FileReaderException;
 import com.sample.factory.FileReaderFactory;
 import com.sample.reader.SampleFileReader;
 
 public class Main {
 
+	private final static Logger LOGGER = Logger.getLogger(Main.class);
+	
 	public static void main(String args[]){
 		
 		FileReaderFactory readerFactory = new FileReaderFactory();
@@ -13,6 +17,7 @@ public class Main {
 		try {
 			
 			reader.readFile();
+			LOGGER.debug("Hello");
 			
 		} catch (FileReaderException e) {
 			// TODO Auto-generated catch block
