255369b04c3e62a72cda818a182a59cc9199e8c4
prabhu-nishant <prabhu.nishant@gmail.com>
2015-07-22 12:17:15 +0530

    Adding Junit test classes

---
+++ sample/src/test/java/com/sample/factory/FileReaderFactoryTest.java
@@ -0,0 +1,14 @@
+package com.sample.factory;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FileReaderFactoryTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
