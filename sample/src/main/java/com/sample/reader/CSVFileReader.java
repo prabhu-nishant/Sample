ba132a972ae3cc299691817dd62a2d9785926b34
Nishant <prabhu.nishant@gmail.com>
2015-07-26 21:00:31 +0530

    Removed print statement

--- sample/src/main/java/com/sample/reader/CSVFileReader.java
+++ sample/src/main/java/com/sample/reader/CSVFileReader.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
@@ -36,7 +35,6 @@ public class CSVFileReader implements SampleFileReader{
 				
 				for(String field:fields){
 					
-					System.out.println(field);
 					String[] temp = field.split(Constants.FIELD_SPLIT_IDENTIFIER);
 					map.putIfAbsent(temp[0], temp[1]);
 				}
